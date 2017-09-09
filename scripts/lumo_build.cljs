d8f3ef5faf342a62520409a758ebb0107c823a2b
Andrea Richiardi <a.richiardi.work@gmail.com>
2017-09-08 18:13:30 -0700

    The inputs function does not want a vector!

--- scripts/lumo_build.cljs
+++ scripts/lumo_build.cljs
@@ -1,6 +1,6 @@
 (require '[lumo.build.api :as b])
 
-(b/build (b/inputs ["src/cljs" "src/js"])
+(b/build (b/inputs "src/cljs" "src/js")
          {:main 'speed-of-light.core
           :output-to "out/speed_of_light.js"
           :optimizations :none
