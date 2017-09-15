4ee7dda7632dc23825c70665cdae15207e60c564
Andrea Richiardi <a.richiardi.work@gmail.com>
2017-09-15 12:56:56 -0700

    Use async callback

--- scripts/lumo_build.cljs
+++ scripts/lumo_build.cljs
@@ -1,9 +1,18 @@
-(require '[lumo.build.api :as b])
+(require '[lumo.build.api :as api])
 
-(b/build "src"
-         {:main 'speed-of-light.core
-          :output-to "out/speed_of_light.js"
-          :optimizations :none
-          :source-map true
-          :target :nodejs
-          :verbose true})
+(def compiler-opts
+  {:main 'speed-of-light.core
+   :output-to "out/speed_of_light.js"
+   :optimizations :none
+   :source-map true
+   :target :nodejs
+   :verbose true})
+
+(api/build
+ (api/inputs "src") ;; variadic
+ compiler-opts
+ (fn [result]
+   (if-let [err (:error result)]
+     (println (.-stack err))
+     (do (println "Complete!")
+         (println result)))))
